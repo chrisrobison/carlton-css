cefa74c5b4375f3f2f0de889b710d44997a2c997
Christopher Robison <cdr@netoasis.net>
2021-02-07 18:22:17 -0800

    Added multiple image support with selector

--- x.php
+++ x.php
@@ -8,30 +8,33 @@
     $cnt = count($lines);
     $all = array();
     for ($i=0; $i<$cnt; $i++) {
-        $line = preg_replace("/^\s*box-shadow:\s*/",'', $lines[$i]);
+        $line = preg_replace("/^\s*box-shadow:\s*/",'', preg_replace("/;/", "", $lines[$i]));
         $parts = preg_split("/\,\s*/", $line);
         $out = array();
         
         $x = 0; $y = 0;
-
+        $row = array();
         for ($c=0; $c<2200; $c++) {
             $tmp = preg_split("/\s/", $parts[$c]);
-//            $x = preg_replace("/0px/", '', $tmp[0]);
-//            $y = preg_replace("/0px/", '', $tmp[1]);
+            $x = intval(preg_replace("/0px/", '', $tmp[0]));
+            $y = intval(preg_replace("/0px/", '', $tmp[1]));
             $color = preg_replace("/transparent/", '', $tmp[2]);
             
-            if (!isset($out[$y])) {
-                $out[$y] = array();
+            $row[] = $color;
+
+            if ($x == 40) {
+                $out[] = $row;
+                $row = array();
             }
-            $out[$y][] = $color;
-            ++$x;
-            if ($x > $cols) {
-                $x = 0;
-                $y++;
-            }
+            // $out[$y][] = $color;
+//            ++$x;
+//            if ($x > $cols) {
+//                $x = 0;
+//                $y++;
+//            }
         }
         $all[] = $out;
     }
     print json_encode($all);
-    print_r($all);
+    //print_r($all);
 ?>
